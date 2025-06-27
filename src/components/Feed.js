da9fa6a529bfc7971a01e1a6a74707a57a7c1522
Rahul Jangra <rjangra4015@gmail.com>
2025-06-27 14:58:59 +0530

    add posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import '../css/Feed.css';
-import QuoraBox from './QuoraBox';
+import React from "react";
+import "../css/Feed.css";
+import QuoraBox from "./QuoraBox";
+import Post from "./Post";
 
 function Feed() {
   return (
-    <div className='feed'>
+    <div className="feed">
       <QuoraBox />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import '../css/Post.css';
+import React from "react";
+import "../css/Post.css";
+import Avatar from "@mui/material/Avatar";
+import { ArrowUpwardOutlined, ArrowDownwardOutlined, ChatBubbleOutlineOutlined, RepeatOutlined, ShareOutlined, MoreHorizOutlined } from "@mui/icons-material";
+import Image from '../images/history.png';
+
 
 function Post() {
   return (
-    <div>
-      
+    <div className="post">
+      <div className="post-info">
+        <Avatar />
+        <h5>Username </h5>
+        <small>Timestamp</small>
+      </div>
+
+      <div className="post-body">
+        <div className="post-que">
+          <p>Question</p>
+          <button className="post-que-ans">Answer</button>
+        </div>
+        <div className="post-ans">
+          <p></p>
+        </div>
+        <img
+          src={Image}
+          alt=""
+        />
+      </div>
+      <div className="post-footer">
+        <div className="post-footer-action">
+          <ArrowUpwardOutlined />
+          <ArrowDownwardOutlined />
+        </div>
+        
+        <RepeatOutlined />
+        <ChatBubbleOutlineOutlined />
+
+        <div className="post-footer-left">
+          <ShareOutlined />
+          <MoreHorizOutlined />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
